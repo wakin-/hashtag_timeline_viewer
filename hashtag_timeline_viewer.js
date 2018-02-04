75109f05b7f8354254da438e42c177673a2abb51
wakin <8th.mail@gmail.com>
2018-02-04 19:49:11 +0900

    initial commit

---
+++ hashtag_timeline_viewer.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Hashtag Timeline Viewer
+// @namespace    
+// @version      0.1.0
+// @description  Mastodonの公開側のハッシュタグタイムラインの拡張
+// @match        https://theboss.tech/tags/theboss_tech
+// @license      The Unlicense
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+})();
